c6b6d480b001ab32eefd40d10946fb3f7855f92b
Michael <ershovme@gmail.com>
2019-01-21 15:45:18 +0800

    Add POST_DETAIL template, view and url

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog_app.models import Post
+from django.shortcuts import render, get_object_or_404
 #import pudb
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.all()
-	#pudb.set_trace()
 	return render(request, 'post_list.html', {
-		'posts': posts
+		'posts': posts,
 		})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'post_detail.html', {'post': post})
