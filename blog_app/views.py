5e7157575452467900605b961bbcf1ca3e27dcd0
Michael <ershovme@gmail.com>
2019-01-21 13:42:45 +0800

    Add templates. Add posts in database

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog_app.models import Post
+#import pudb
 
 # Create your views here.
+def post_list(request):
+	posts = Post.objects.all()
+	#pudb.set_trace()
+	return render(request, 'post_list.html', {
+		'posts': posts
+		})
